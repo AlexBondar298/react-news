c981d66cde12b1086d4b17976ba8dcd4d535a371
AlexBondar <alexbondar298@gmail.com>
2024-02-23 16:17:58 +0200

    pipec_230224

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import TableCart from "./components/Table/TableCart";
 
 interface DataNews {
-  author: null | string;
+  author: string;
   title: string;
-  description: null | string;
+  description: string;
   url?: string;
   urlToImage: string;
   publishedAt?: string;
@@ -15,15 +15,22 @@ interface DataNews {
 
 function App() {
   const [dataNews, setDataNews] = useState<DataNews[]>([]);
+  const [nonRepeatingArticles, setNonRepeatingArticles] = useState<DataNews[]>([]); //*удаление дубликатов статей
+
   const [lengthArticles, setLengthArticles] = useState<number>(10);
 
+  const [searchNews, setSearchNews] = useState<string>("");
+  const [searchItem, setSearchItem] = useState<keyof DataNews>("title");
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isPinNews, setIsPinNews] = useState<boolean>(true);
+
   /************************************************************** */
   useEffect(() => {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const dataResponse: any = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-01-22&sortBy=publishedAt&apiKey=685ecc34aee24e8da83f408a24c9b783");
+        const dataResponse: any = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-01-23&sortBy=publishedAt&apiKey=685ecc34aee24e8da83f408a24c9b783");
         setDataNews(dataResponse.data.articles);
         setIsLoading(false);
       } catch (error) {
@@ -34,14 +41,42 @@ function App() {
     fetchData();
   }, []);
 
+  //************************************************************* */
+  const myNews: DataNews[] = [
+    {
+      author: "author",
+      title: "title",
+      description: "description",
+      urlToImage: "https://www.hibridosyelectricos.com/uploads/s1/63/37/18/tesla-semi-pepsi-2_17_2000x1126.jpeg",
+    },
+  ];
+
+  const addMyNews = (lengthArticles: number): void => {
+    setNonRepeatingArticles((prev) => [...prev.slice(0, lengthArticles), myNews[0], ...prev.slice(lengthArticles)]);
+    setLengthArticles((prev) => prev + 1);
+  };
+
+  //************************************************************* */
+
   //*********************************************************** */
-  const nonRepeatingArticles: DataNews[] = [];
   for (let i of dataNews) {
     if (!nonRepeatingArticles.find((article) => article.title === i.title)) {
-      nonRepeatingArticles.push(i);
+      if (i.title !== "[Removed]") nonRepeatingArticles.push(i);
     }
   }
-  //************************************************************* */
+
+  const pinNewsByTitle = (title: string): void => {
+    const pinNewsArr: DataNews[] = nonRepeatingArticles.filter((elem) => elem.title === title);
+    if (isPinNews) {
+      const otherNewsArr: DataNews[] = nonRepeatingArticles.filter((elem) => elem.title !== title);
+      setDataNews([...pinNewsArr, ...otherNewsArr]);
+      setIsPinNews(!isPinNews);
+    } else {
+      pinNewsArr[0].title === dataNews[0].title && setIsPinNews(!isPinNews);
+    }
+  };
+  console.log(nonRepeatingArticles);
+
   return (
     <div className="body">
       <header className="header">
@@ -56,12 +91,22 @@ function App() {
               ) : (
                 <>
                   <h1 className="table-title title">All articles about Tesla from the last month</h1>
+                  <div className="table__search">
+                    <input type="text" value={searchNews} onChange={(event) => setSearchNews(event.target.value)} placeholder="search" />
+                    <input type="radio" name="searcNewsUser" value="title" checked={searchItem === "title" ? true : false} onChange={(event: any) => setSearchItem(event.target.value)} /> {"title"}
+                    <input type="radio" name="searcNewsUser" value="description" checked={searchItem === "description" ? true : false} onChange={(event: any) => setSearchItem(event.target.value)} />
+                    {"description"}
+                  </div>
                   <ul className="table__list">
-                    {nonRepeatingArticles.slice(0, lengthArticles).map((elem, index) => (
-                      <TableCart key={index} author={elem.author} title={elem.title} description={elem.description} urlToImage={elem.urlToImage} />
-                    ))}
+                    {nonRepeatingArticles
+                      .slice(0, lengthArticles)
+                      .filter((elem) => elem[searchItem]?.toLocaleLowerCase().includes(searchNews.toLocaleLowerCase()))
+                      .map((elem, index) => (
+                        <TableCart key={index} author={elem.author} title={elem.title} description={elem.description} urlToImage={elem.urlToImage} pinNewsByTitle={() => pinNewsByTitle(elem.title)} />
+                      ))}
                   </ul>
-                  <div className="addNews">
+                  <div className="table__addNews">
+                    <button onClick={() => addMyNews(lengthArticles)}> Add my News </button>
                     <button onClick={() => setLengthArticles((prev) => prev + 10)}> Add news</button>
                   </div>
                 </>
--- src/components/Table/TableCart.tsx
+++ src/components/Table/TableCart.tsx
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface DataCart {
-  author: null | string;
+  author: string;
   title: string;
-  description: null | string;
+  description: string;
   urlToImage: string;
+  pinNewsByTitle: () => void;
 }
 
-const TableCart = ({ author, title, description, urlToImage }: DataCart) => {
+const TableCart = ({ author, title, description, urlToImage, pinNewsByTitle }: DataCart) => {
+  const [editAuthor, setAuthor] = useState<string>(author);
+  const [editTitle, setTitle] = useState<string>(title);
+  const [editDescription, setDescription] = useState<string>(description);
+  const [editUrlToImage, setUrlToImage] = useState<string>(urlToImage);
+
+  const [editWord, setEditWord] = useState<string>("Edit...");
+
+  const [edit, setEdit] = useState<boolean>(false);
+
+  const editNews = (elem: any, arg:string): void => {
+    setEdit(!edit);
+    setEditWord(elem);
+    if (arg === "editTitle") setTitle(editWord);
+    if (arg === "editDescription") setDescription(editWord);
+    if (arg === "editAuthor") setAuthor(editWord);
+    if (arg === "editUrlToImage") setUrlToImage(editWord);
+  };
+
   return (
     <li className="table__cart">
-      <h2 className="table__cart-title">{title}</h2>
-      <img src={urlToImage} alt="image" className="table__cart-img" />
-      <p className="table__cart-description">{description}</p>
-      <p className="table__cart-author">
-        Author: <span>{author}</span>
-      </p>
+      <div className="table__cart-item">
+        <h2 className="table__cart-title" onClick={() => editNews(editTitle, "editTitle")}>
+          {editTitle}
+        </h2>
+        <img src={editUrlToImage} alt="image" className="table__cart-img" onClick={() => editNews(editUrlToImage, "editUrlToImage")}/>
+        <p className="table__cart-description" onClick={() => editNews(editDescription, "editDescription")}>{editDescription}</p>
+        <p className="table__cart-author" onClick={() => editNews(editAuthor, "editAuthor")}>
+          Author: <span>{editAuthor}</span>
+        </p>
+      </div>
+      {edit ? (
+        <>
+          <div>
+            <textarea value={editWord} onChange={(event: any) => setEditWord(event.target.value)} />
+          </div>
+        </>
+      ) : null}
+      <div className="table__cart-pin">
+        <button onClick={pinNewsByTitle}> Pin news </button>
+      </div>
     </li>
   );
 };
